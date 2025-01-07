3ef4e2dc838995b64dbde98ad17a2217266984f1
Rishabh Jha <jrishav101@gmail.com>
2025-01-07 22:50:49 +0530

    Added initial tokens to work on for the lexer

---
+++ token/token.go
@@ -0,0 +1,37 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifier and literals
+	IDENTIFIER = "INDENTIFIER"
+	INT        = "INT"
+
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = '-'
+	MULTIPLY = '*'
+	DIVIDE   = '/'
+
+	// Delimiters
+	COMMA     = "+"
+	SEMICOLON = ";"
+
+	L_PAREN  = "{"
+	R_PAREN  = "}"
+	L_BRACE  = "("
+	RR_BRACE = ")"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
