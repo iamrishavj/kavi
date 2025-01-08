e4cb4bc42938fa639f12cdecac73de187577e491
Rishabh Jha <jrishav101@gmail.com>
2025-01-08 20:34:25 +0530

    Added read character to the lexer with state

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -9,5 +9,18 @@ type Lexer struct {
 
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
+	l.readChar()
 	return l
+}
+
+func (lexer *Lexer) readChar() {
+	if lexer.readPosition > len(lexer.input) {
+		lexer.ch = 0
+	} else {
+		lexer.ch = lexer.input[lexer.readPosition]
+	}
+
+	lexer.position = lexer.readPosition
+	lexer.readPosition += 1
+
 }
