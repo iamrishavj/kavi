7c2b57410cd1c39f1b77799c8ab0f602aede10f6
Rishabh Jha <jrishav101@gmail.com>
2025-01-11 23:15:24 +0530

    Corrected test case for the updated lexer

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -12,29 +12,53 @@ type expectedToken struct {
 }
 
 func TestNextToken(t *testing.T) {
-	input := `=+(){},;-*/`
-	tests := []expectedToken{
+	input := `let five = 5;
+	let ten = 10;
+	let add = fn(x, y) {
+	x + y;
+	};
+	let result = add(five, ten);
+	`
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.LET, "let"},
+		{token.IDENTIFIER, "five"},
 		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENTIFIER, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENTIFIER, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
 		{token.L_PAREN, "("},
+		{token.IDENTIFIER, "x"},
+		{token.COMMA, ","},
+		{token.IDENTIFIER, "y"},
 		{token.R_PAREN, ")"},
 		{token.L_BRACE, "{"},
-		{token.R_BRACE, "}"},
-		{token.COMMA, ","},
+		{token.IDENTIFIER, "x"},
+		{token.PLUS, "+"},
+		{token.IDENTIFIER, "y"},
 		{token.SEMICOLON, ";"},
-		{token.MINUS, "-"},
-		{token.MULTIPLY, "*"},
-		{token.DIVIDE, "/"},
+		{token.R_BRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENTIFIER, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENTIFIER, "add"},
+		{token.L_PAREN, "("},
+		{token.IDENTIFIER, "five"},
+		{token.COMMA, ","},
+		{token.IDENTIFIER, "ten"},
+		{token.R_PAREN, ")"},
+		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
-	l := New(input)
-
-	for i, tt := range tests {
-		token := l.NextToken()
-
-		if token.Type != tt.expectedType {
-			t.Fatalf("test[%d] - literal wrong. \nExpected=%q, got=%q", i, tt.expectedType, token.Literal)
-		}
-	}
-
 }
